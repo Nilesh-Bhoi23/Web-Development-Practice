f29096e2275c88608a925d9c865a265e98ae201f
Nilesh-bhoi23 <nileshbhoi9201@gmail.com>
2024-02-28 22:03:37 +0530

    JavaScript Introduction

---
+++ Practice 54/script.js
@@ -0,0 +1,23 @@
+let btn1 = document.querySelector("#btn1");
+ 
+btn1.onclick = ()=> {
+    console.log("Button was clicked");
+
+} 
+
+alert('Hii welcome to my Web-site');
+
+console.log('Code is running.....')
+console.log('Code is also running.....')
+console.log('your Number is #54')
+
+var a = prompt("Enter the number : ");
+var isTrue = confirm("are you sure you want to leave this page and blast your computer ");
+
+if(isTrue) {
+    console.log("your computer is blast");
+}
+
+else {
+    console.log("your computer is Not blast: ");
+}
