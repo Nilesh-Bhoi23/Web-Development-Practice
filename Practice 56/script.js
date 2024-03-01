81be812f4fb2b56685d57f8f73885159c4c73d62
Nilesh-bhoi23 <nileshbhoi9201@gmail.com>
2024-03-01 23:27:00 +0530

    56

---
+++ Practice 56/script.js
@@ -0,0 +1,30 @@
+console.log("hii how are you?");
+
+let age = 18;
+// let grace = 2;
+
+// console.log(age + grace);
+// console.log(age - grace);
+// console.log(age * grace);
+// console.log(age / grace);
+// console.log(age ** grace);
+// console.log(age % grace);
+
+// if((age+grace)> 18) {
+//     console.log("you can drive");
+// }
+
+// else {
+//     console.log("you can not drive");
+// }
+
+// age += grace;
+// console.log(age);
+
+if(age == 18) {
+    console.log("you can drive");
+}
+
+else {
+    console.log("you can not drive");
+}
