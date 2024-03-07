0d3840aee31535ac8911b8bb7366ca1a32aadc1a
Nilesh Bhoi <147185281+Nilesh-Bhoi23@users.noreply.github.com>
2024-03-07 21:29:52 +0530

    Add files via upload

---
+++ Video 58/index1.js
@@ -0,0 +1,57 @@
+// function nice(name) {
+//     console.log("hey " + name + " you are nice");
+//     console.log("hey " + name + " you are good");
+//     console.log("hey " + name + " you are tshirt good");
+// }
+
+// nice("Manish");
+// nice("Nilesh");
+
+
+
+// function sum(a,b) {
+//     console.log(a + b);
+// }
+// sum(8,8);
+
+// --------------------------------------------------
+
+// function sum(a,b) {
+//     // console.log(a + b);
+//     return a + b;
+// }
+// result1 = sum(8,9);
+// result2 = sum(7,8);
+// result3 = sum(8,60);
+
+// console.log("The sum of this numbers is :"+result1);
+// console.log("The sum of this numbers is :"+result2);
+// console.log("The sum of this numbers is :"+result3);
+
+// --------------------------------------------------------
+
+// function sum(a,b, c =3) {
+//     // console.log(a + b);
+//     return a + b + c;  
+// }
+// result1 = sum(8,9);
+// result2 = sum(7,8);
+// result3 = sum(8,60,1); // optional c  parameter passed 1
+
+// console.log("The sum of this numbers is :"+result1);
+// console.log("The sum of this numbers is :"+result2);
+// console.log("The sum of this numbers is :"+result3);
+
+// when value is NaN means value is undefined..
+
+// Arrow function 
+
+const func1 = (x)=> {
+    console.log("i am an arrow function", x)
+}
+
+func1(34);
+func1(54);
+func1(84);
+
+
